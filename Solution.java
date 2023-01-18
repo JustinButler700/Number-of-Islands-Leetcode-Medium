954d3e75d9feca01a312ad063a19d9ab80e7ca38
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2023-01-18 11:11:36 -0500

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    boolean[][] seen; // boolean array to keep track of visited islands
+
+    public int numIslands(char[][] grid) {
+        // Keep count of how many unique islands we have seen
+        int islands = 0;
+        
+        // when we encounter a new island, we will map it with dfs, and 
+        // update a boolean array to represent the island
+        seen = new boolean[grid.length][grid[0].length];
+        
+        // iterate through the grid
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j]=='1'&&!seen[i][j]){
+                    islands++;
+                    // dfs map of our newly discovered island
+                    connect(i, j, grid);
+                }
+            }
+        }
+        return islands;
+    }
+    
+    // DFS function to map out the island
+    public void connect(int x, int y, char[][] grid){
+        if(seen[x][y]){return;}
+        seen[x][y]=true;
+        
+        // check north
+        if(x-1 >= 0 && !seen[x-1][y] && grid[x-1][y]=='1'){
+            connect(x-1, y, grid);
+        }
+        // check west
+        if(y-1 >= 0 && !seen[x][y-1] && grid[x][y-1]=='1'){
+            connect(x, y-1, grid);
+        }
+        // check south
+        if(x+1 < grid.length && !seen[x+1][y] && grid[x+1][y]=='1'){
+            connect(x+1, y, grid);
+        }
+        // check east
+        if(y+1 < grid[0].length && !seen[x][y+1] && grid[x][y+1]=='1'){
+            connect(x, y+1, grid);
+        }
+
+    }
+}
